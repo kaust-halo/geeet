e9a90aaf7bf94aa3dceacb72c74a10ae19193fca
Oliver Lopez <lopezv.oliver@gmail.com>
2024-02-01 08:39:09 +0300

    join-datetime

--- geeet/eepredefined/join.py
+++ geeet/eepredefined/join.py
@@ -3,19 +3,22 @@ Optional module to define some useful ee functions to join specific collections.
 """
 import ee
 
-# Add a datetime property to the landsat data in the same format as ECMWF
-def add_ECMWF_datetime(img):
+def set_datetime(img):
+    """
+    Adds a "datetime" property in the format YYYYMMDDTHH (e.g. 20150101T07)
+    """
     d = img.get('system:time_start')
     d_parsed = ee.Date(d).format("yyyyMMdd'T'HH")
     img = img.set({'system:datetime': d_parsed})
     return img
 
 def landsat_ecmwf(Sat_collection, Meteo_collection):
-    sat_data = Sat_collection.map(add_ECMWF_datetime)
+    sat_data = Sat_collection.map(set_datetime)
+    met_data = Meteo_collection.map(set_datetime)
 
     # Join the two collections using the datetime property. 
-    filterByDateTime = ee.Filter.equals(leftField='system:datetime', rightField='system:index')
-    joinByDateTime = ee.ImageCollection(ee.Join.inner().apply(sat_data, Meteo_collection, filterByDateTime))
+    filterByDateTime = ee.Filter.equals(leftField='system:datetime', rightField='system:datetime')
+    joinByDateTime = ee.ImageCollection(ee.Join.inner().apply(sat_data, met_data, filterByDateTime))
     def get_img(feature):
         return ee.Image.cat(feature.get('primary'), feature.get('secondary'))
 
